88f36a18e0e0facfa3d8adaab2de73e5a78e645c
robhertt <robhertt@gmail.com>
2024-05-15 20:46:02 -0600

    Primera tarea de Progra 01

---
+++ src/mi/primera/aplicacion/MiPrimeraAplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mi.primera.aplicacion;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author BigBoss
+ */
+public class MiPrimeraAplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(parentComponent, args);
+    }
+    
+}
